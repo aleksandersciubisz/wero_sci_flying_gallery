d0fb84f4aaefd5236b48edbe76aec9d68ce2350e
aleksandersciubisz <aleksander.sciubisz@gmail.com>
2024-02-01 21:20:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const images = document.getElementsByClassName('image')
+
+let globalIndex = 0,
+	last = { x: 0, y: 0 }
+
+const activate = (image, x, y) => {
+	image.style.left = `${x}px`
+	image.style.top = `${y}px`
+	image.style.zIndex = globalIndex
+
+	image.dataset.status = 'active'
+	if (image.dataset.status == 'active') {
+		image.classList.remove('dim')
+	}
+
+	last = { x, y }
+}
+
+const distanceFromLast = (x, y) => {
+	return Math.hypot(x - last.x, y - last.y)
+}
+
+const handleOnMove = (e) => {
+	if (distanceFromLast(e.clientX, e.clientY) > window.innerWidth / 15) {
+		const lead = images[globalIndex % images.length],
+			tail = images[(globalIndex - 5) % images.length]
+		dim = images[(globalIndex - 1) % images.length]
+
+		activate(lead, e.clientX, e.clientY)
+
+		if (tail) tail.dataset.status = 'inactive'
+		if (dim) dim.classList.add('dim')
+
+		globalIndex++
+	}
+}
+
+window.onmousemove = (e) => handleOnMove(e)
+
+window.ontouchmove = (e) => handleOnMove(e.touches[0])
